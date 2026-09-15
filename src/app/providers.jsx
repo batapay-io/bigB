567154c7f4fc451c520148f63d32bc6cb7baa4d4
Ansh Rastogi <thisisanshrastogi@gmail.com>
2026-09-16 00:00:23 +0530

    studio logs disabled

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -8,7 +8,19 @@ if (typeof window !== 'undefined') {
     person_profiles: 'always',
   })
 }
+import { usePathname } from 'next/navigation'
+import { useEffect } from 'react'
 
 export function CSPostHogProvider({ children }) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (pathname.startsWith('/studio')) {
+      posthog.opt_out_capturing();
+    } else {
+      posthog.has_opted_out_capturing() && posthog.opt_in_capturing();
+    }
+  }, [pathname]);
+
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
